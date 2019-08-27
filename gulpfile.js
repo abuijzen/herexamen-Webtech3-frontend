13ac4ddcbb714a102dd24992abf7ec02878d4e31
Angelique <angeliquebuijzen@hotmail.com>
2019-08-27 14:17:54 +0200

    browsersync gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,5 +10,30 @@ function style(){
     .pipe(sass())
     //de map waarin je de gecompilede css wilt opslaan
     .pipe(gulp.dest('./css'))
+
+    //browsersync
+    .pipe(browserSync.stream());
 }
+
+//automatisch watchen voor wijzigingen
+function watch(){
+  browserSync.init({
+    //launch server
+    server:{
+      //base directory
+      baseDir: './'
+    }
+  });
+  //veranderingen watchen in elke scss file
+  //en de functie style oproepen voor compiling
+  gulp.watch('./scss/**/*.scss',style);
+
+  //html en js watchen
+  //all html en js files in dezelfde map
+  //browser refreshen als er veranderingen zijn
+  gulp.watch('./*.html').on('change',browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change',browserSync.reload);
+}
+  
   exports.style = style;
+  exports.watch = watch;
